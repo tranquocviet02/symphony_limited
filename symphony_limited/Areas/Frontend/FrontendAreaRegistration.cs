adedb70f0e15a806aa40fbc0b51a139f823feefc
= <=>
2021-12-20 20:26:23 +0700

    Project 3

---
+++ symphony_limited/Areas/Frontend/FrontendAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace symphony_limited.Areas.Frontend
+{
+    public class FrontendAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Frontend";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Frontend_default",
+                "Frontend/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
